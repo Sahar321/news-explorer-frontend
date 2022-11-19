7b942d07551d1b20ccda5df2befb970a9dfded90
Sahar Moshe <saharm123456@gmail.com>
2022-11-20 00:19:59 +0200

    create main component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -6,8 +6,8 @@ import './Main.css';
 export default function Main() {
   return (
     <main className="main">
-      <h1 className="title__main"> What&apos;s going on in the world?</h1>
-      <p className="title__subtext">
+      <h1 className="main__title"> What&apos;s going on in the world?</h1>
+      <p className="main__subtext">
         Find the latest news on any topic and save them in your personal
         account.
       </p>

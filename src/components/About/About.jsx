a1a98ef2bd1316937136f136020de7f58c4f5709
Sahar Moshe <saharm123456@gmail.com>
2022-11-19 20:09:20 +0200

    ..

---
+++ src/components/About/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import authorImage from '../../images/about/author-about.png';
+import './About.css';
+
+export default function About() {
+  return (
+    <section className="about">
+      <div className="about__text-container">
+        <h2 className="about__title">About the author</h2>
+        <p className="about__text">
+          This block describes the project author. Here you should indicate your
+          name, what you do, and which development technologies you know.<br/><br/> You
+          can also talk about your experience with Practicum, what you learned
+          there, and how you can help potential customers.
+        </p>
+      </div>
+      <img src={authorImage} alt="author" className="about__image" />
+    </section>
+  );
+}

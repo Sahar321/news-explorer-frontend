c9d6a4540f00a9883dc6d154d3645608ead90379
Sahar Moshe <saharm123456@gmail.com>
2022-11-22 01:43:19 +0200

    add onSignInClick to header

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,11 +4,10 @@ import Header from '../components/Header/Header.jsx';
 import Main from '../components/Main/Main.jsx';
 import Footer from '../components/Footer/Footer.jsx';
 
-export default function Home({ loggedIn }) {
-  console.log(loggedIn);
+export default function Home({ OnSignInClick }) {
   return (
     <div className="app">
-      <Header />
+      <Header OnSignInClick={OnSignInClick} />
       <Main />
       <Footer />
     </div>

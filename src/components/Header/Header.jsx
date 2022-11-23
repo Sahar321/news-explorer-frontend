c3cf73dc56766d51eb502c4ebdd961aded16f7a6
Sahar Moshe <saharm123456@gmail.com>
2022-11-23 06:03:45 +0200

    add active class to navlink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,20 +7,32 @@ import './Header.css';
 /* eslint react/prop-types: 0 */
 /* eslint no-confusing-arrow: 0 */
 /* eslint  implicit-arrow-linebreak : 0 */
-/* className={({ isActive }) => (isActive ? 'header__nav-item' : 'header__nav-item')} */
+/* className={({ isActive }) => (isActive ? 'header__nav-link' : 'header__nav-link')} */
 export default function Header({ OnSignInClick }) {
+  const defaultClass = 'header__nav-link';
+  const activeClass = 'header__nav-link header__nav-link_isActive_true';
   return (
     <header className="header">
       <h1 className="header__title">NewsExplorer</h1>
       <nav className="header__nav">
         <ul className="header__nav-list">
           <li className="header__nav-item">
-            <NavLink to="/" className="header__nav-link">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive ? activeClass : defaultClass
+              }
+            >
               Home
             </NavLink>
           </li>
           <li className="header__nav-item">
-            <NavLink to="/SavedArticles" className="header__nav-link">
+            <NavLink
+              to="/SavedArticles"
+              className={({ isActive }) =>
+                isActive ? activeClass : defaultClass
+              }
+            >
               Saved articles
             </NavLink>
           </li>

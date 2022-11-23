50f28197fa3e42f8b371b10aebb3891be9fec76b
Sahar Moshe <saharm123456@gmail.com>
2022-11-23 05:52:18 +0200

    a -> navlink

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 /* eslint react/prop-types: 0 */
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 // import { Routes, Route } from 'react-router-dom';
 // import SavedNews from '../SavedNews/SavedNews.jsx';
 import './Header.css';
-
+/* eslint react/prop-types: 0 */
+/* eslint no-confusing-arrow: 0 */
+/* eslint  implicit-arrow-linebreak : 0 */
+/* className={({ isActive }) => (isActive ? 'header__nav-item' : 'header__nav-item')} */
 export default function Header({ OnSignInClick }) {
   return (
     <header className="header">
@@ -11,14 +15,14 @@ export default function Header({ OnSignInClick }) {
       <nav className="header__nav">
         <ul className="header__nav-list">
           <li className="header__nav-item">
-            <a href="#" className="header__nav-link">
+            <NavLink to="/" className="header__nav-link">
               Home
-            </a>
+            </NavLink>
           </li>
           <li className="header__nav-item">
-            <a href="#" className="header__nav-link">
+            <NavLink to="/SavedArticles" className="header__nav-link">
               Saved articles
-            </a>
+            </NavLink>
           </li>
         </ul>
         <button onClick={OnSignInClick} className="button button__sign-in">

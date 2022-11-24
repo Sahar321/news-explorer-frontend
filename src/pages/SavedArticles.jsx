9a1d9674d542ec043d10d53d813eef37a7b80cf2
Sahar Moshe <saharm123456@gmail.com>
2022-11-24 08:34:40 +0200

    change add app_page_saved-articles type to main app div

--- src/pages/SavedArticles.jsx
+++ src/pages/SavedArticles.jsx
@@ -1,7 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function SavedArticles() {
   // const [loggedIn, setLoggedIn] = React.useState(false);
+  useEffect(() => {
+    const app = document.querySelector('.app');
+    app.classList.add('app_page_saved-articles');
+    return () => {
+      app.classList.remove('app_page_saved-articles');
+    };
+  }, []);
   return (
     <>
       <h1>Saved Articles</h1>

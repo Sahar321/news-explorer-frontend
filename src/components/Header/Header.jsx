890a9cf5d50b540fab397d371848d997f08494b0
Sahar Moshe <saharm123456@gmail.com>
2022-11-26 15:21:20 +0200

    improve selected page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,26 +1,26 @@
 /* eslint react/prop-types: 0 */
 /* eslint no-unused-vars: 0 */
 /* eslint operator-linebreak: 0 */
-
-import React from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
-import signOutImage from '../../images/icons/sign-out.svg';
-// import { Routes, Route } from 'react-router-dom';
-// import SavedNews from '../SavedNews/SavedNews.jsx';
-import './Header.css';
 /* eslint react/prop-types: 0 */
 /* eslint no-confusing-arrow: 0 */
 /* eslint  implicit-arrow-linebreak : 0 */
-/* className={({ isActive }) => (isActive ? 'header__nav-link' : 'header__nav-link')} */
+import React, { useEffect } from 'react';
+import { NavLink, useLocation } from 'react-router-dom';
+import './Header.css';
+
 export default function Header({ OnSignInClick, OnSignOutClick, loggedIn }) {
+  const [selectedPage, setSelectedPage] = React.useState('');
   const defaultClass = 'header__nav-link';
   const activeClass = 'header__nav-link header__nav-link_isActive_true';
   const { pathname } = useLocation();
-  const savedArticlesClass =
-    pathname === '/SavedArticles' && 'header_page_saved-articles';
+
+  useEffect(() => {
+    if (pathname === '/') setSelectedPage('header_page_home');
+    if (pathname === '/SavedArticles') setSelectedPage('header_page_saved-articles');
+  }, [pathname]);
 
   return (
-    <header className={`header ${savedArticlesClass}`}>
+    <header className={`header ${selectedPage}`}>
       <h1 className="header__title">NewsExplorer</h1>
       <nav className="header__nav">
         <ul className="header__nav-list">
@@ -50,7 +50,7 @@ export default function Header({ OnSignInClick, OnSignOutClick, loggedIn }) {
             onClick={OnSignOutClick}
             className="button button_type_sign-out"
           >
-            elise <img alt="Sign out" src={signOutImage} />
+            elise <i className="icon icon_type_sign-out"></i>
           </button>
         ) : (
           <button onClick={OnSignInClick} className="button button__sign-in">

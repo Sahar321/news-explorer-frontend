b0c85e2c09bfd4af42c3a4951bacb662592ee59c
Sahar Moshe <saharm123456@gmail.com>
2022-11-28 01:42:36 +0200

    add card top buttons

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -12,11 +12,12 @@ export default function NewsCard() {
   return (
     <article className="NewsCard">
       <div className="NewsCard__image-container">
+        <div className="newsCard__buttons-wrapper">
+          <button className="button button_type_remove" />
+          <button className="button newsCard__tooltip">Remove from saved</button>
+          <span className="newsCard__keyword">Nature</span>
+        </div>
         <img className="NewsCard__image" src={imgBeach} alt="beach" />
-        <button
-          type="button"
-          className="button button-bookmark button-bookmark_state_marked"
-        ></button>
       </div>
       <div className="NewsCard__text-container">
         <p className="NewsCard__date">November 4, 2020</p>
@@ -25,13 +26,12 @@ export default function NewsCard() {
         </h3>
         <p className="NewsCard__text">
           Ever since I read Richard Louvs influential book, Last Child in the
-          Woods, the idea of having a special sit spot has stuck with me.
-          This advice, which Louv attributes to nature educator Jon Young, is
-          for both adults and children to find...
+          Woods, the idea of having a special sit spot has stuck with me. This
+          advice, which Louv attributes to nature educator Jon Young, is for
+          both adults and children to find...
         </p>
         <p className="NewsCard__source">treehugger</p>
       </div>
     </article>
-
   );
 }

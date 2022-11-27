b63f12e6a517334a63e69f62da11cbe1bd1f1201
Sahar Moshe <saharm123456@gmail.com>
2022-11-27 12:05:56 +0200

    and saved Articles

--- src/pages/SavedArticles.jsx
+++ src/pages/SavedArticles.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import NewsCardList from '../components/NewsCardList/NewsCardList.jsx';
 
 export default function SavedArticles() {
   // const [loggedIn, setLoggedIn] = React.useState(false);
@@ -11,7 +12,13 @@ export default function SavedArticles() {
   }, []);
   return (
     <>
-      <h1>Saved Articles</h1>
+      <section className="saved-articles">
+        <h1 className="saved-articles__title">Saved Articles</h1>
+        <h2 className="saved-articles__subtitle">Elise, you have 5 saved articles</h2>
+        <h3 className='saved-articles__keywords'>By keywords: Nature, Yellowstone, and 2 other</h3>
+      </section>
+
+      <NewsCardList />
     </>
   );
 }

487a26d06227fdf8df36571d25a019a9d3918da7
Sahar Moshe <saharm123456@gmail.com>
2022-11-27 12:06:51 +0200

    add showTitle arg

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import './NewsCardList.css';
 import NewsCard from '../NewsCard/NewsCard.jsx';
 
-export default function NewsCardList() {
+export default function NewsCardList({ showTitle }) {
   return (
     <section className="NewsCardList">
-      <h2 className="NewsCardList__text-results">Search results</h2>
-        <div className="NewsCardList__card-container">
-          <NewsCard />
-          <NewsCard />
-          <NewsCard />
-
-        </div>
-        <button type="button" className='button button__show-more'>Show More</button>
+      <h2 className={`NewsCardList__text-results NewsCardList__text-results_isVisible_${showTitle}`}>
+        Search results
+      </h2>
+      <div className="NewsCardList__card-container">
+        <NewsCard />
+        <NewsCard />
+        <NewsCard />
+      </div>
+      <button type="button" className="button button__show-more">
+        Show More
+      </button>
     </section>
   );
 }

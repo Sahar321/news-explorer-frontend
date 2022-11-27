089f9750beec03251ddfe7bb0812611d267fe37b
Sahar Moshe <saharm123456@gmail.com>
2022-11-27 12:07:03 +0200

    add showTitle

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -13,7 +13,7 @@ export default function Main() {
         account.
       </p>
       <SearchForm />
-      <NewsCardList />
+      <NewsCardList showTitle={true} />
       <About />
     </main>
   );

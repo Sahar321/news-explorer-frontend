73c68da17615fa4f73a73b9a39aa97a37ee0a9a8
Sahar Moshe <saharm123456@gmail.com>
2022-11-28 12:11:35 +0200

    Rearranging according to BMI

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -10,28 +10,27 @@ import imgRiver from '../../images/cards/river.png';
  */
 export default function NewsCard() {
   return (
-    <article className="NewsCard">
-      <div className="NewsCard__image-container">
-        <div className="newsCard__buttons-wrapper">
+    <article className="card">
+      <div className="card__image-wrapper">
+        <div className="card__controls-wrapper">
           <button className="button button_type_remove" />
-          <button className="button newsCard__tooltip">Remove from saved</button>
-          <span className="newsCard__keyword">Nature</span>
+          <span className="card__tooltip">Remove from saved</span>
+          <span className="card__keyword">Nature</span>
         </div>
-        <img className="NewsCard__image" src={imgBeach} alt="beach" />
+        <img className="card__image" src={imgBeach} alt="beach" />
       </div>
-      <div className="NewsCard__text-container">
-        <p className="NewsCard__date">November 4, 2020</p>
-        <h3 className="NewsCard__title">
-          Everyone Needs a Special sit spot in Nature
-        </h3>
-        <p className="NewsCard__text">
-          Ever since I read Richard Louvs influential book, Last Child in the
-          Woods, the idea of having a special sit spot has stuck with me. This
-          advice, which Louv attributes to nature educator Jon Young, is for
-          both adults and children to find...
-        </p>
-        <p className="NewsCard__source">treehugger</p>
-      </div>
+
+      <p className="card__date">November 4, 2020</p>
+      <h3 className="card__title">
+        Everyone Needs a Special sit spot in Nature
+      </h3>
+      <p className="card__text">
+        Ever since I read Richard Louvs influential book, Last Child in the
+        Woods, the idea of having a special sit spot has stuck with me. This
+        advice, which Louv attributes to nature educator Jon Young, is for both
+        adults and children to find...
+      </p>
+      <p className="card__source">treehugger</p>
     </article>
   );
 }

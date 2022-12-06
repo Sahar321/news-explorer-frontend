77113e075c9409b8305b85596459268310bf9de6
Sahar Moshe <saharm123456@gmail.com>
2022-12-06 19:11:04 +0200

    NewsCardList according to page

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -1,21 +1,33 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import './NewsCardList.css';
 import NewsCard from '../NewsCard/NewsCard.jsx';
 
-export default function NewsCardList({ showTitle }) {
+export default function NewsCardList() {
+  const { pathname } = useLocation();
+  const isHomePage = pathname === '/';
+  const selectedPage = isHomePage ? 'home' : 'saved-articles';
+
   return (
     <section className="card-list">
-      <h2 className={`card-list__search-results card-list__text-search_isVisible_${showTitle}`}>
+      <h2
+        className={`card-list__search-results card-list__search-results_isVisible_${isHomePage}`}
+      >
         Search results
       </h2>
-      <div className="card-list__cards-wrapper card-list__cards-wrapper_page_saved-articles">
+      <div
+        className={`card-list__cards-wrapper card-list__cards-wrapper_page_${selectedPage}`}
+      >
+        <NewsCard />
         <NewsCard />
         <NewsCard />
         <NewsCard />
       </div>
-      <button type="button" className="button button__show-more">
-        Show More
-      </button>
+      {isHomePage && (
+        <button type="button" className="button button__show-more">
+          Show more
+        </button>
+      )}
     </section>
   );
 }

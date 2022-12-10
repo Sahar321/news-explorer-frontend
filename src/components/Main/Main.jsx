baae1743be1668ab7448ec8d777596e5af94c952
Sahar Moshe <saharm123456@gmail.com>
2022-12-10 17:55:10 +0200

    adding container for main heading

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -7,12 +7,15 @@ import './Main.css';
 export default function Main() {
   return (
     <main className="main">
-      <h1 className="main__title"> What&apos;s going on in the world?</h1>
-      <p className="main__subtext">
-        Find the latest news on any topic and save them in your personal
-        account.
-      </p>
-      <SearchForm />
+      <div className="main__heading">
+        <h1 className="main__title"> What&apos;s going on in the world?</h1>
+        <p className="main__subtext">
+          Find the latest news on any topic and save them in your personal
+          account.
+        </p>
+        <SearchForm />
+      </div>
+
       <NewsCardList showTitle={true} />
       <About />
     </main>

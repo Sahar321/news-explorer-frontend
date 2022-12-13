43420d662b8b759541610315584df63e7077ab3c
Sahar Moshe <saharm123456@gmail.com>
2022-12-14 01:42:47 +0200

    fix nav menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,49 +5,85 @@
 /* eslint no-confusing-arrow: 0 */
 /* eslint  implicit-arrow-linebreak : 0 */
 import React, { useEffect } from 'react';
-import { NavLink, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import Navigation from '../Navigation/Navigation.jsx';
 import useScreenWidth from '../../utils/hooks/useScreenWidth';
+import { MOBILE_MENU_WIDTH } from '../../constants/constants';
 import './Header.css';
 
-export default function Header({
-  OnSignInClick,
-  OnSignOutClick,
-  loggedIn,
-  navMenuIsOpen,
-}) {
-  const mobileWidth = 590; // need to move to constants
-  const [selectedPage, setSelectedPage] = React.useState('');
-  const { pathname } = useLocation();
-  const [isNavMenuOpen, setIsNavMenuOpen] = React.useState(false);
+export default function Header({ OnSignInClick, hideMobileMenuButton }) {
   const screenWidth = useScreenWidth();
-
+  const { pathname } = useLocation();
+  const [selectedPage, setSelectedPage] = React.useState('');
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
+  const [isMobileType, setIsMobileType] = React.useState(false);
+  const [itemsMobileColor, setItemsMobileColor] = React.useState('');
+  const [mobileMenuButtonColor, setMobileMenuButtonColor] = React.useState('');
+  const [isMobileMenuButtonVisible, setIsMobileMenuButtonVisible] =
+    React.useState(false);
   useEffect(() => {
-    if (pathname === '/') setSelectedPage('header_page_home');
-    if (pathname === '/SavedArticles') {
-      setSelectedPage('header_page_saved-articles');
+    switch (pathname) {
+      case '/':
+        setSelectedPage('header_page_home');
+        setMobileMenuButtonColor('header__nav_mobile_white');
+        break;
+      case '/SavedArticles':
+        setMobileMenuButtonColor('header__nav_mobile_black');
+        setSelectedPage('header_page_saved-articles');
+        setItemsMobileColor('navigation__items_color_white');
+        break;
+      default:
+        break;
     }
   }, [pathname]);
 
-  useEffect(() => {
-    if (screenWidth > mobileWidth) {
-      setIsNavMenuOpen(false);
-    }
-    console.log('screenWidth', screenWidth);
-  }, [screenWidth]);
-  const handleMenuClick = (e) => {
-    setIsNavMenuOpen(!isNavMenuOpen);
+  const handleSignInClick = () => {
+    setIsMobileMenuOpen(false);
+    OnSignInClick();
   };
-  const menuClass = isNavMenuOpen ? 'header__title_menu_white' : '';
+
+  useEffect(() => {
+    if (screenWidth > MOBILE_MENU_WIDTH) {
+      setIsMobileMenuButtonVisible(false);
+      setIsMobileType(false);
+      if (isMobileMenuOpen) {
+        setIsMobileMenuOpen(false);
+      }
+    } else {
+      setIsMobileType(true);
+      setIsMobileMenuButtonVisible(true);
+    }
+  }, [screenWidth]);
+
+  const handleMenuClick = (e) => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  const isMobileMenuButtonClass =
+    isMobileMenuButtonVisible && !hideMobileMenuButton && 'header__nav_type_mobile_visible';
+  const headerMobileMenuClass = isMobileMenuOpen && 'header__title_menu_white';
+
+  const handleItemClick = () => {
+    setIsMobileMenuOpen(false);
+  };
+
   return (
     <header className={`header ${selectedPage}`}>
-      <h1 className={`header__title ${menuClass}`}>NewsExplorer</h1>
+      <h1 className={`header__title ${headerMobileMenuClass}`}>NewsExplorer</h1>
       <button
         type="button"
-        className="button header__nav_type_mobile"
+        className={`button header__nav_type_mobile ${isMobileMenuButtonClass} ${
+          isMobileMenuOpen ? 'button_type_close' : mobileMenuButtonColor
+        }`}
         onClick={handleMenuClick}
       />
-      <Navigation showNav={isNavMenuOpen} />
+      <Navigation
+        OnSignInClick={handleSignInClick}
+        showMobileMenu={isMobileMenuOpen}
+        isMobileType={isMobileType}
+        itemsMobileColor={itemsMobileColor}
+        onItemClick={handleItemClick}
+      />
     </header>
   );
 }

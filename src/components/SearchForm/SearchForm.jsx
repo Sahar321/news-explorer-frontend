77685689117fcedd7aac3696e250b1a262edd4e5
Sahar321 <Saharm123456@gmail.com>
2022-12-19 17:33:06 +0200

    made controller component

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './SearchForm.css';
 
-export default function SearchForm() {
+export default function SearchForm({ onSearchSumbit }) {
+  const [keyword, setKeyword] = useState('');
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    onSearchSumbit(keyword);
+  };
   return (
-    <form className="search-form">
-      <input maxLength="50" className="input input__search" type="text" placeholder="Enter topic" />
+    <form onSubmit={handleSumbit} className="search-form">
+      <input onChange={(event) => setKeyword(event.target.value)} maxLength="50" className="input input__search" type="text" placeholder="Enter topic" />
       <button aria-label="Search" className="button button__search" type="submit">
         Search
       </button>

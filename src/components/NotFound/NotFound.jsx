6918778cb57c2985efd488e745247e4d03a91cb0
Sahar321 <Saharm123456@gmail.com>
2022-12-19 16:15:23 +0200

    new component

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import './NotFound.css';
 
 export default function NotFound() {
   return (
-    <>
-      <h1>Resource Not Found</h1>
-    </>
+    <div className='not-found'>
+      <i className='not-found__icon'></i>
+      <h3 className='not-found__title'>Nothing found</h3>
+      <span className='not-found__text'>
+        Sorry, but nothing matched<br />your search terms.
+      </span>
+    </div>
   );
 }

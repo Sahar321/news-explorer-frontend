672fbe54aaa25321908775b19f46c7c67e54aba1
Sahar Moshe <saharm123456@gmail.com>
2023-02-12 20:29:50 +0200

    add config for api url address

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
-const localhost = 'http://localhost:3000';
+const localhost = 'http://localhost:3001';
 const server = 'https://api.samnews.students.nomoredomainssbs.ru';
 
 export const BASE_URL = localhost;

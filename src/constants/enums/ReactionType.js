17cb43012474da3ea023c41724b5a6a865fb7830
Sahar Moshe <saharm123456@gmail.com>
2023-03-24 18:32:22 +0300

    change icons

--- src/constants/enums/ReactionType.js
+++ src/constants/enums/ReactionType.js
@@ -1,8 +1,8 @@
-import likeIcon from '../../images/icons/reactions/popup/like.svg';
-import lolIcon from '../../images/icons/reactions/popup/lol.svg';
-import loveIcon from '../../images/icons/reactions/popup/love.svg';
-import sadIcon from '../../images/icons/reactions/popup/sad.svg';
-import wowIcon from '../../images/icons/reactions/popup/wow.svg';
+import likeIcon from '../../images/icons/reactions/popup/like.png';
+import lolIcon from '../../images/icons/reactions/popup/lol.png';
+import loveIcon from '../../images/icons/reactions/popup/love.png';
+import sadIcon from '../../images/icons/reactions/popup/sad.png';
+import wowIcon from '../../images/icons/reactions/popup/wow.png';
 // implement
 const ReactionType = {
   LOL: lolIcon,

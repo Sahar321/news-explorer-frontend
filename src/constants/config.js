aaebf2ae1a2f1066ca424e9d36355ab5dfcab965
Sahar Moshe <saharm123456@gmail.com>
2023-05-20 18:33:24 +0300

    fix https

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
 const localhost = 'http://localhost:3001';
-const server = 'http://api.news.saharm.com';
+const server = 'https://api.news.saharm.com';
 
 export const BASE_URL = server;

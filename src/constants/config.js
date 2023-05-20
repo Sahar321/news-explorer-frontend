8a74cdbf66cfdc8e170ccc36e05381fcbd28ea60
Sahar Moshe <saharm123456@gmail.com>
2023-05-20 15:48:09 +0300

    change to server

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,5 @@
 /*eslint-disable */
 const localhost = 'http://localhost:3001';
-const server = 'https://api.samnews.students.nomoredomainssbs.ru';
+const server = 'http://api.news.saharm.com';
 
-export const BASE_URL = localhost;
+export const BASE_URL = server;

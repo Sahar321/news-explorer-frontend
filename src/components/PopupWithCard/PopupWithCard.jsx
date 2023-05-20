ba2749f85743e81e9df2de065006d7266b125910
Sahar Moshe <saharm123456@gmail.com>
2023-05-20 15:47:37 +0300

    add onThankYou

--- src/components/PopupWithCard/PopupWithCard.jsx
+++ src/components/PopupWithCard/PopupWithCard.jsx
@@ -15,6 +15,7 @@ export default function PopupWithCard({
   isOpen,
   onCommentSubmit,
   comments,
+  onThankYou,
 }) {
   const screenWidth = useScreenWidth();
   useCloseOnEscape(isOpen, onClose);
@@ -65,6 +66,7 @@ export default function PopupWithCard({
                 key={index}
                 index={index + 1}
                 comment={comment}
+                onThankYou={onThankYou}
               />
             ))}
           </div>

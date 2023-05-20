490c99bc51b237beba4c636698b375e5cf1854f9
Sahar Moshe <saharm123456@gmail.com>
2023-05-20 15:47:53 +0300

    fix number display

--- src/components/ReactionStats/ReactionStats.jsx
+++ src/components/ReactionStats/ReactionStats.jsx
@@ -4,34 +4,57 @@ import totalPostIcon from '../../images/icons/totalpost.png';
 import thankyouIcon from '../../images/icons/thankyouIcon.png';
 import './ReactionStats.css';
 
-export default function ReactionStats(data) {
-  const { reactionId, totalThankYouCount, totalPostCount } = data;
-  const [statsDate, setStatsDate] = useState(data);
+export default function ReactionStats({ stats }) {
+  const { reactionId } = stats;
+  const [totalComments, setTotalComments] = useState(0);
+  const [totalThankYou, setTotalThankYou] = useState(0);
 
-  function formatNumber(num) {
-    const formattingRules = {
-      T: 1000000000000000000,
-      B: 1000000000000,
-      M: 1000000,
+  /*   const formatUserStatsData = (stats) => {
+console.log(stats);
+  };
+ */
+  function formatNumberWithLetterShortCut(number) {
+    const letterNumberShortcut = {
       K: 1000,
-    };
-    Object.entries(formattingRules).forEach(([suffix, value]) => {
-      if (num >= value) {
-        var formattedNum = (num / value).toFixed(1) + suffix;
-        return formattedNum;
-      }
-    });
+      M: 1000000,
+      B: 1000000000,
 
-    return num;
+    };
+
+    for (const [key, value] of Object.entries(letterNumberShortcut)) {
+      if (number >= value) {
+        return `${(number / value).toFixed(1)}${key}+ `;
+      }
+    }
+
+    return number.toString();
+  }
+  function formatNumberWithLetterShortcut(number) {
+    const abbreviations = [
+      { value: 1e12, symbol: 'Holy Moses' },
+      { value: 1e9, symbol: 'B' },
+      { value: 1e6, symbol: 'M' },
+      { value: 1e3, symbol: 'K' },
+    ];
+
+    for (let i = 0; i < abbreviations.length; i++) {
+      if (number >= abbreviations[i].value) {
+        const scaledNumber = number / abbreviations[i].value;
+        const formattedNumber = scaledNumber.toFixed(1);
+        return formattedNumber + abbreviations[i].symbol +"+";
+      }
+    }
+
+    // if the number is smaller than 1000, return it as is
+    return number.toString();
   }
 
-  const formatData = (data) => {};
-
   useEffect(() => {
-
-    setStatsDate(formatData(data));
-  }, [data]);
-
+    const comments = formatNumberWithLetterShortcut(stats.totalComments);
+    const thankYou = formatNumberWithLetterShortcut(stats.totalThankYou);
+    setTotalComments(comments);
+    setTotalThankYou(thankYou);
+  }, [stats]);
   return (
     <>
       <div className="reaction-stats">
@@ -40,7 +63,7 @@ export default function ReactionStats(data) {
           className="popup__reaction__count-icon"
           alt=""
         />
-        <span className="reaction-stats__total-count">30h+</span>
+        <span className="reaction-stats__total-count">{totalComments}</span>
       </div>
       <div className="reaction-stats na">
         <img
@@ -48,7 +71,7 @@ export default function ReactionStats(data) {
           className="popup__reaction__count-icon"
           alt=""
         />
-        <span className="reaction-stats__total-count">30h+</span>
+        <span className="reaction-stats__total-count">{totalThankYou}</span>
       </div>
       <i
         className={`icon reaction-stats__reaction icon_reaction_${reactionId?.toLowerCase()}`}

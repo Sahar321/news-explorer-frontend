4dc3bf2d6c016426c6d3cb9d83b82aabf8005781
Sahar <saharm123456@gmail.com>
2023-07-29 14:37:41 +0300

    change url

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,5 +1,6 @@
 /*eslint-disable */
 const localhost = 'http://localhost:3001';
 const server = 'https://api.news.saharm.com';
+const localHostServer = 'http://192.168.1.61:3001';
 
-export const BASE_URL = server;
+export const BASE_URL = localHostServer;

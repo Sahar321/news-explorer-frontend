a9782df24c0e23e102512b023e31de44c9ec7680
Sahar <saharm123456@gmail.com>
2023-07-29 14:33:23 +0300

    remove comment button

--- src/components/PopupWithCard/PopupWithCard.jsx
+++ src/components/PopupWithCard/PopupWithCard.jsx
@@ -2,13 +2,14 @@
 import React, { useState } from 'react';
 import Comment from '../Comment/Comment.jsx';
 import ChatMessage from '../ChatMessage/ChatMessage.jsx';
-import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
-import Button from '@mui/material/Button';
-import { SCREEN_WIDTHS } from '../../constants/constants';
+
 import useCloseOnEscape from '../../utils/hooks/useCloseOnEscape';
 import './PopupWithCard.css';
 import useScreenWidth from '../../utils/hooks/useScreenWidth';
-import InputLabel from '@mui/material/InputLabel';
+import NewsCard from '../NewsCard/NewsCard.jsx';
+
+import { Divider } from '@mui/material';
+
 export default function PopupWithCard({
   cardData,
   onClose,
@@ -16,29 +17,40 @@ export default function PopupWithCard({
   onCommentSubmit,
   comments,
   onThankYou,
+  onReactionSelect,
+  onUniqueReactionsClick,
+  onRemoveReaction,
+  onCardBookmarkClick,
+  onCardRemoveClick,
+  bookmarkCards,
+  loggedIn,
+  onCardShare,
 }) {
   const screenWidth = useScreenWidth();
   useCloseOnEscape(isOpen, onClose);
-  const [isWriteCommentOpen, setIsWriteCommentOpen] = useState(false);
-  const { TABLET_SIZE_WIDTH } = SCREEN_WIDTHS;
-  const isScreenSmallerThanTablet = screenWidth < TABLET_SIZE_WIDTH;
 
-  const renderButtonComment = () => {
-    if (!isScreenSmallerThanTablet) return null;
-    const handleCommentButton = () => {
-      setIsWriteCommentOpen(!isWriteCommentOpen);
-    };
-    return (
-      <Button onClick={handleCommentButton} className="comment__write-button">
-        <HistoryEduIcon className="comment__write-icon" />
-        {isWriteCommentOpen ? 'Close Comment' : ''}
-      </Button>
-    );
-  };
+
+
   const handleOnCommentSubmit = (value) => {
     const { link } = cardData;
     onCommentSubmit({ link, text: value });
     setIsWriteCommentOpen(false);
+  };
+
+  const cardClasses = {
+    card: 'card_type_popup',
+    imageWrapper: 'card__image-wrapper_type_popup',
+    image: 'card__image_type_popup',
+    textWrapper: 'card__text-wrapper_type_popup',
+    text: 'card__text_type_popup',
+    title: 'card__title_type_popup',
+  };
+
+  const elementsToHide = {
+    comment: true,
+    title: true,
+    date: true,
+    source: true,
   };
 
   return (
@@ -50,30 +62,45 @@ export default function PopupWithCard({
           type="button"
           onClick={onClose}
         ></button>
-        <h2 className="popup__title popup__title_type_card">Card Date</h2>
+        <h2 className="card__title popup__title_type_card">
+          {`${cardData.source} - ${cardData.date}`}
+        </h2>
 
+        <NewsCard
+          cardData={cardData}
+          elementsToHide={elementsToHide}
+          classList={cardClasses}
+          onReactionSelect={onReactionSelect}
+          onUniqueReactionsClick={onUniqueReactionsClick}
+          onRemoveReaction={onRemoveReaction}
+          onCardBookmarkClick={onCardBookmarkClick}
+          onCardRemoveClick={onCardRemoveClick}
+          bookmarkCards={bookmarkCards}
+          loggedIn={loggedIn}
+          onCardShare={onCardShare}
+        />
+        <Divider />
+        <Divider />
         <div className="popup__comments">
-          {renderButtonComment()}
-
-          <InputLabel id="demo-simple-select-label">Sort</InputLabel>
-          <Comment
-            onCommentSubmit={handleOnCommentSubmit}
-            isOpen={isScreenSmallerThanTablet && isWriteCommentOpen}
-          />
           <div className="message-list">
+            {!comments.length > 0 && (
+              <h3 className="message-list__empty-title">No comments yet...</h3>
+            )}
+
             {comments?.map((comment, index) => (
-              <ChatMessage
-                key={index}
-                index={index + 1}
-                comment={comment}
-                onThankYou={onThankYou}
-              />
+              <>
+                <ChatMessage
+                  key={index}
+                  index={index + 1}
+                  comment={comment}
+                  onThankYou={onThankYou}
+                />
+              </>
             ))}
           </div>
-        </div>
-        {screenWidth > TABLET_SIZE_WIDTH && (
+
           <Comment onCommentSubmit={handleOnCommentSubmit} isOpen={true} />
-        )}
+        </div>
       </div>
     </div>
   );

44eb4ae851206d10dc1c82836379fbd37d1af4fa
Sahar <saharm123456@gmail.com>
2023-07-30 12:22:40 +0300

    fix reactions

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,5 +1,5 @@
 /*eslint-disable*/
-import React, { useEffect } from 'react';
+import React, { useDebugValue, useEffect } from 'react';
 import './NewsCard.css';
 import CardType from '../../constants/enums/CardType';
 import Button from '@mui/material/Button';
@@ -11,6 +11,9 @@ import AddReactionIcon from '@mui/icons-material/AddReaction';
 import ShareIcon from '@mui/icons-material/Share';
 import CommentIcon from '@mui/icons-material/Comment';
 import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+import { formatNumberWithLetterShortcut } from '../../utils/helpers';
+import anime from 'animejs/lib/anime.es.js';
+import 'animate.css';
 export default function NewsCard({
   cardData,
   cardType,
@@ -31,7 +34,8 @@ export default function NewsCard({
   const [selectedReaction, setSelectedReaction] = React.useState(
     <AddReactionIcon />
   );
-  const { keyword, title,  description, date, source, link, image, reaction } =
+
+  const { keyword, title, description, date, source, link, image, reaction } =
     cardData;
   const handleOnCardShare = () => {
     onCardShare(cardData);
@@ -44,7 +48,6 @@ export default function NewsCard({
     if (!userCardReaction) return;
     handleSetReaction(userCardReaction.reactionId);
   }, [cardData.reaction]);
-
 
   const isBookmark = bookmarkCards?.includes(link) ? true : false;
   const isBookmarkActiveClass = isBookmark
@@ -59,7 +62,11 @@ export default function NewsCard({
   const handleCardRemoveClick = () => {
     onCardRemoveClick(cardData);
   };
-
+  useEffect(() => {
+    if (isBookmark) {
+      console.log('cardData', cardData);
+    }
+  }, [isBookmark]);
   const bookmarkButton = (
     <>
       <button
@@ -109,8 +116,8 @@ export default function NewsCard({
 
   const handleRemoveReaction = () => {
     setSelectedReaction(<AddReactionIcon />);
-    setIsReactionsOpen(false);
     onRemoveReaction(cardData);
+    setIsReactionsOpen(false);
   };
 
   const handleOnCommentClick = (e) => {
@@ -120,6 +127,18 @@ export default function NewsCard({
   const handleOnUniqueReactionsClick = () => {
     onUniqueReactionsClick(cardData);
   };
+
+  useEffect(() => {
+    if (!isReactionsOpen) return;
+/*     anime({
+      targets: "#WOW,#LOL,#LIKE,#SAD,#LOVE",
+      scale: [
+        {value: .1, easing: 'easeInElastic', duration: 0},
+        {value: 1, easing: 'easeInElastic', duration: 400}
+      ],
+      delay: anime.stagger(200, {grid: [5, 1], from: 'center'})
+    }); */
+  }, [isReactionsOpen]);
 
   return (
     <article className={`card ${classList?.card ? classList.card : ''}`}>
@@ -146,56 +165,85 @@ export default function NewsCard({
         )}
       </div>
       <Divider />
-      <div className={`reactions reactions_visible_${isReactionsOpen}`}>
-        <img
-          alt="lol"
-          id="LOL"
-          onClick={handleReactionsClick}
-          width="30px"
-          src={ReactionType['LOL']}
-        />
-        <img
-          alt="wow"
-          id="WOW"
-          onClick={handleReactionsClick}
-          width="30px"
-          src={ReactionType['WOW']}
-        />
-        <img
-          alt="LIKE"
-          id="LIKE"
-          onClick={handleReactionsClick}
-          width="30px"
-          src={ReactionType['LIKE']}
-        />
-        <img
-          alt="SAD"
-          id="SAD"
-          onClick={handleReactionsClick}
-          width="30px"
-          src={ReactionType['SAD']}
-        />
-        <img
-          alt="love"
-          id="LOVE"
-          onClick={handleReactionsClick}
-          width="30px"
-          src={ReactionType['LOVE']}
-        />
-      </div>
+
       <div className="card__reactions-warper">
+        <div  className={`reactions  reactions_visible_${isReactionsOpen}`}>
+          <svg
+            id="closeRections"
+            xmlns="http://www.w3.org/2000/svg"
+            width="40"
+            height="40"
+            fill="currentColor"
+            onClick={handleRemoveReaction}
+          >
+            <path
+              fill="currentColor"
+              d="m22.357 20 8.821 8.821-2.357 2.357L18.35 20.707a1 1 0 0 1 0-1.414L28.82 8.82l2.357 2.357L22.357 20Z"
+            />
+            <path
+              fill="currentColor"
+              d="M18.13 20 9.31 28.82l2.356 2.357 10.472-10.471a1 1 0 0 0 0-1.414L11.666 8.82 9.31 11.178 18.131 20Z"
+            />
+          </svg>
+          <img
+            alt="lol"
+            id="LOL"
+            onClick={handleReactionsClick}
+            width="30px"
+            className='animate__animated animate__bounceIn'
+            src={ReactionType['LOL']}
+          />
+          <img
+            alt="wow"
+            id="WOW"
+            onClick={handleReactionsClick}
+            width="30px"
+            className='animate__animated animate__bounceIn'
+            src={ReactionType['WOW']}
+          />
+          <img
+            alt="LIKE"
+            id="LIKE"
+            onClick={handleReactionsClick}
+            width="30px"
+            className='animate__animated animate__bounceIn'
+            src={ReactionType['LIKE']}
+          />
+          <img
+            alt="SAD"
+            id="SAD"
+            onClick={handleReactionsClick}
+            width="30px"
+            className='animate__animated animate__bounceIn'
+            src={ReactionType['SAD']}
+          />
+          <img
+            alt="love"
+            id="LOVE"
+            onClick={handleReactionsClick}
+            width="30px"
+            className='animate__animated animate__bounceIn'
+            src={ReactionType['LOVE']}
+          />
+        </div>
         <Button onClick={handleOnCardShare}>
           <ShareIcon />
         </Button>
         {!elementsToHide?.comment ? (
           <Button onClick={handleOnCommentClick}>
-            <CommentIcon />
+            <CommentIcon />{' '}
+            <span className="reaction__text-button">
+              {formatNumberWithLetterShortcut(cardData?.comments?.length)}
+            </span>
           </Button>
         ) : (
           ''
         )}
-        <Button onDoubleClick={handleRemoveReaction} onClick={handleReactions}>
+        <Button onClick={handleReactions}>
           {selectedReaction}
+          <span className="reaction__text-button">
+            {formatNumberWithLetterShortcut(cardData?.reaction?.length)}
+          </span>
         </Button>
       </div>
       <Divider />
@@ -207,14 +255,17 @@ export default function NewsCard({
         {!elementsToHide?.date ? <p className="card__date">{date}</p> : ''}
 
         <h3
-        dir='auto'
+          dir="auto"
           className={`card__title ${classList?.title ? classList.title : ''}`}
         >
           {title}
         </h3>
 
-        <p    dir='auto' className={`card__text ${classList?.text ? classList.text : ''}`}>
-        {description}
+        <p
+          dir="auto"
+          className={`card__text ${classList?.text ? classList.text : ''}`}
+        >
+          {description}
         </p>
         <a
           className="card__source"
--- src/components/ReactionStats/ReactionStats.jsx
+++ src/components/ReactionStats/ReactionStats.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from 'react';
 import totalPostIcon from '../../images/icons/totalpost.png';
 import thankyouIcon from '../../images/icons/thankyouIcon.png';
+import {formatNumberWithLetterShortcut} from '../../utils/helpers';
 import './ReactionStats.css';
 
 export default function ReactionStats({ stats }) {
@@ -9,44 +10,7 @@ export default function ReactionStats({ stats }) {
   const [totalComments, setTotalComments] = useState(0);
   const [totalThankYou, setTotalThankYou] = useState(0);
 
-  /*   const formatUserStatsData = (stats) => {
-console.log(stats);
-  };
- */
-  function formatNumberWithLetterShortCut(number) {
-    const letterNumberShortcut = {
-      K: 1000,
-      M: 1000000,
-      B: 1000000000,
-    };
 
-    for (const [key, value] of Object.entries(letterNumberShortcut)) {
-      if (number >= value) {
-        return `${(number / value).toFixed(1)}${key}+ `;
-      }
-    }
-
-    return number.toString();
-  }
-  function formatNumberWithLetterShortcut(number) {
-    const abbreviations = [
-      { value: 1e12, symbol: 'Holy Moses' },
-      { value: 1e9, symbol: 'B' },
-      { value: 1e6, symbol: 'M' },
-      { value: 1e3, symbol: 'K' },
-    ];
-
-    for (let i = 0; i < abbreviations.length; i++) {
-      if (number >= abbreviations[i].value) {
-        const scaledNumber = number / abbreviations[i].value;
-        const formattedNumber = scaledNumber.toFixed(1);
-        return formattedNumber + abbreviations[i].symbol + '+';
-      }
-    }
-
-    // if the number is smaller than 1000, return it as is
-    return number.toString();
-  }
 
   useEffect(() => {
     const comments = formatNumberWithLetterShortcut(stats.totalComments);

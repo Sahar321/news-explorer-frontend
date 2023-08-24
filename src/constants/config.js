65971cea73c09970ce2a7dbace1c79d8125581d8
Sahar Moshe <saharm123456@gmail.com>
2023-08-25 01:59:36 +0300

    .

--- src/constants/config.js
+++ src/constants/config.js
@@ -3,4 +3,4 @@ const localhost = 'http://localhost:3001';
 const server = 'https://api.news.saharm.com';
 const localHostServer = 'http://192.168.1.61:3001';
 
-export const BASE_URL = localhost;
+export const BASE_URL = server;

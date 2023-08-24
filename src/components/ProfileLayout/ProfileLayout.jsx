a2931db3ca4b80133b4fc8fcabeed69596560f70
Sahar Moshe <saharm123456@gmail.com>
2023-08-24 11:56:04 +0300

    transferred  them to the component folder

--- src/components/ProfileLayout/ProfileLayout.jsx
+++ src/components/ProfileLayout/ProfileLayout.jsx
@@ -1,12 +1,14 @@
 /*eslint-disable*/
 import { Routes, Route, Link, useLocation, Navigate, } from 'react-router-dom';
 import React, { useEffect, useContext, useState } from 'react';
+import CurrentUserContext from '../../contexts/CurrentUserContext';
 
-import Profile from '../pages/Profile.jsx';
+import Profile from '../../pages/Profile.jsx';
 import { NavLink } from 'react-router-dom';
 import './ProfileLayout.css';
-const ProfileLayout = ({ handleAvatarClick, setAppStyles, onProfileEdit }) => {
-  const classNameFunc = ({ isActive }) =>
+const ProfileLayout = ({ handleAvatarClick, setAppStyles, onProfileEditClick }) => {
+  const currentUser = useContext(CurrentUserContext);
+  const handleLinkIsActive = ({ isActive }) =>
     isActive ? 'menu__item-link active-link' : 'menu__item-link';
   useEffect(() => {
     setAppStyles('app_page_saved-articles');
@@ -19,17 +21,17 @@ const ProfileLayout = ({ handleAvatarClick, setAppStyles, onProfileEdit }) => {
       <nav className="menu">
         <ul className="menu__warper">
           <li className="menu__item">
-            <NavLink end className={classNameFunc} to="/profile">
+            <NavLink end className={handleLinkIsActive} to="/profile">
               Profile
             </NavLink>
           </li>
           <li className="menu__item">
-            <NavLink end className={classNameFunc} to="/profile/comments">
+            <NavLink end className={handleLinkIsActive} to="/profile/comments">
               Comments
             </NavLink>
           </li>
           <li className="menu__item">
-            <NavLink end to="/profile/reactions" className={classNameFunc}>
+            <NavLink end to="/profile/reactions" className={handleLinkIsActive}>
               Reactions
             </NavLink>
           </li>
@@ -40,9 +42,10 @@ const ProfileLayout = ({ handleAvatarClick, setAppStyles, onProfileEdit }) => {
           path="/"
           element={
             <Profile
-            onProfileEdit={onProfileEdit}
+            onProfileEditClick={onProfileEditClick}
               onAvatarClick={handleAvatarClick}
               setAppStyles={setAppStyles}
+              currentUser={currentUser}
             />
           }
         />

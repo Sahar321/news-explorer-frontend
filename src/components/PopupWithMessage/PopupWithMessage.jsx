13bb7aaf53f2fd269d1c214a8280fe76ee3aeddb
Sahar Moshe <saharm123456@gmail.com>
2023-09-18 13:00:39 +0300

    .

--- src/components/PopupWithMessage/PopupWithMessage.jsx
+++ src/components/PopupWithMessage/PopupWithMessage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/*eslint-disable*/
+import React, { useEffect } from 'react';
 import useCloseOnEscape from '../../utils/hooks/useCloseOnEscape';
 
 import './PopupWithMessage.css';
@@ -8,27 +9,39 @@ export default function Popup({
   isOpen,
   title,
   children,
+  id,
+  closeOnOutsideClick = true,
+  isCloseButtonVisible = true,
+  className,
 }) {
   useCloseOnEscape(isOpen, onClose);
   const handleOverlayClose = (evt) => {
-    const popup = evt.target.classList;
-    if (popup.contains('popup')) {
+    if (!closeOnOutsideClick) return;
+    const itsId = evt.target.id === id;
+    const popupClassList = evt.target.classList;
+
+    if (popupClassList.contains('popup') && itsId) {
       onClose();
     }
   };
 
   return (
     <div
+      id={id}
       className={`popup popup_isVisible_${isOpen}`}
       onClick={handleOverlayClose}
     >
-      <div className="popup__container popup__container_type_message">
-        <button
-          aria-label="Close Message Popup"
-          className="button button_type_close popup__button-close"
-          type="button"
-          onClick={onClose}
-        ></button>
+      <div
+        className={`popup__container popup__container_type_message  ${className}`}
+      >
+        {isCloseButtonVisible && (
+          <button
+            aria-label="Close Message Popup"
+            className="button button_type_close popup__button-close"
+            type="button"
+            onClick={onClose}
+          ></button>
+        )}
         <h2 className="popup__title">{title}</h2>
 
         {children}

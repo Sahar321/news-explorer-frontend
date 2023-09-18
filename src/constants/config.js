cb003ce4886a74d794cc44f32782276c46132e9f
Sahar Moshe <saharm123456@gmail.com>
2023-09-18 13:02:29 +0300

    .

--- src/constants/config.js
+++ src/constants/config.js
@@ -1,4 +1,7 @@
 /*eslint-disable */
 const localhost = 'http://localhost:3001';
+const server = 'https://api.news.saharm.com';
+
 
 export const BASE_URL = process.env.REACT_APP_BASE_URL || localhost;
+//export const BASE_URL = server

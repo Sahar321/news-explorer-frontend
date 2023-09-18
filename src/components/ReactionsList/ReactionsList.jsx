ba3fd4974c0c3fd3d8b19aed06e2f42b4ae27eac
Sahar Moshe <saharm123456@gmail.com>
2023-09-18 13:01:47 +0300

    fix hasReactions

--- src/components/ReactionsList/ReactionsList.jsx
+++ src/components/ReactionsList/ReactionsList.jsx
@@ -10,25 +10,31 @@ export default function ReactionsList({
   onUniqueReactionsClick,
   classList,
 }) {
-
-
+  const [hasReactions, setHasReactions] = React.useState(false);
+  useEffect(() => {
+    setHasReactions(Object.keys(reactionsCountByType).length > 0);
+  }, []);
 
   return (
-    <div className={`reactions__list ${classList}`}>
-      {Object.entries(reactionsCountByType).map(([type, count]) => (
-        <button
-          key={type}
-          aria-label={`${type} count`}
-          className="button button_type_reaction-item"
-          onClick={onUniqueReactionsClick}
-        >
-          <span className="reactions__count">{count}</span>
+    <>
+      {hasReactions && (
+        <div className={`reactions__list ${classList}`}>
+          {Object.entries(reactionsCountByType).map(([type, count]) => (
+            <button
+              key={type}
+              aria-label={`${type} count`}
+              className="button button_type_reaction-item"
+              onClick={onUniqueReactionsClick}
+            >
+              <span className="reactions__count">{count}</span>
 
-          <i
-            className={`icon icon-reaction icon_reaction_${type.toLowerCase()}`}
-          ></i>
-        </button>
-      ))}
-    </div>
+              <i
+                className={`icon icon-reaction icon_reaction_${type.toLowerCase()}`}
+              ></i>
+            </button>
+          ))}
+        </div>
+      )}
+    </>
   );
 }

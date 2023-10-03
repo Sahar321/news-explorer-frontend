45e4a6a56cc145bf359e1aae2840b4d97c7bdd64
Sahar Moshe <saharm123456@gmail.com>
2023-10-03 13:56:35 +0300

    remove comments

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,6 +14,7 @@ export default function Header({
   loggedIn,
   className,
 }) {
+  const handleItemClick = () => {console.log('handleItemClick');};
   return (
     <header className={`header ${className}`}>
       <span className={`header__title `}>NewsExplorer</span>
@@ -26,83 +27,8 @@ export default function Header({
         onSignInClick={onSignInClick}
         onSignOut={onSignOut}
         loggedIn={loggedIn}
+        onItemClick={handleItemClick}
       />
     </header>
   );
 }
-
-/*
-
-
-    ${selectedPage}
-   ${headerMobileMenuClass}
-  ${isMobileMenuButtonClass}
-  ${isMobileMenuOpen ? 'button_type_close' : mobileMenuButtonColor}
-
-  showMobileMenu={isMobileMenuOpen}
-  isMobileType={isMobileType}
-  itemsMobileColor={itemsMobileColor}
-  onItemClick={handleItemClick}
-const screenWidth = useScreenWidth();
-  const { pathname } = useLocation();
-  const { MOBILE_MENU_WIDTH } = SCREEN_WIDTHS;
-  const [selectedPage, setSelectedPage] = useState('');
-
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
-  const [isMobileType, setIsMobileType] = useState(false);
-  const [itemsMobileColor, setItemsMobileColor] = useState('');
-  const [mobileMenuButtonColor, setMobileMenuButtonColor] = useState('');
-  const [isMobileMenuButtonVisible, setIsMobileMenuButtonVisible] =
-    useState(false);
-  useEffect(() => {
-    switch (pathname) {
-      case '/':
-        setSelectedPage('header_page_home');
-        setMobileMenuButtonColor('header_mobile_white');
-        break;
-      case '/SavedArticles':
-        setMobileMenuButtonColor('header_mobile_black');
-        setSelectedPage('header_page_saved-articles');
-        setItemsMobileColor('navigation__items_color_white');
-        break;
-      case '/profile':
-        setMobileMenuButtonColor('header_mobile_black');
-        setSelectedPage('header_page_saved-articles');
-        setItemsMobileColor('navigation__items_color_white');
-        break;
-      default:
-        break;
-    }
-  }, [pathname]);
-
-  const handleSignInClick = () => {
-    setIsMobileMenuOpen(false);
-    onSignInClick();
-  };
-
-  useEffect(() => {
-    if (screenWidth > MOBILE_MENU_WIDTH) {
-      setIsMobileMenuButtonVisible(false);
-      setIsMobileType(false);
-      if (isMobileMenuOpen) {
-        setIsMobileMenuOpen(false);
-      }
-    } else {
-      setIsMobileType(true);
-      setIsMobileMenuButtonVisible(true);
-    }
-  }, [screenWidth]);
-
-  const handleMenuClick = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
-  };
-
-  const isMobileMenuButtonClass =
-    isMobileMenuButtonVisible &&
-    !hideMobileMenuButton &&
-    'header_mobile_visible';
-  const headerMobileMenuClass = isMobileMenuOpen && 'header__title_menu_white';
-
-  const handleItemClick = () => {
-    setIsMobileMenuOpen(false);
-  }; */
